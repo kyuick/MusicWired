56017ebe330b32cc3312809183f66fd1b008b083
kyuick <rbdlr369@naver.com>
2022-06-22 13:37:26 +0900

    Member, Music 테이블 수정해서 DTO 및 해당 내용들 모두 수정

--- src/main/java/com/icia/musicwired/dto/MemberDTO.java
+++ src/main/java/com/icia/musicwired/dto/MemberDTO.java
@@ -17,66 +17,111 @@ public class MemberDTO {
 
 	private MultipartFile mProfile;
 	private String mProfileName;
+	private String mNickName;
+	private int mPoint;
+
 	public String getmId() {
 		return mId;
 	}
+
 	public void setmId(String mId) {
 		this.mId = mId;
 	}
+
 	public String getmPw() {
 		return mPw;
 	}
+
 	public void setmPw(String mPw) {
 		this.mPw = mPw;
 	}
+
 	public String getmName() {
 		return mName;
 	}
+
 	public void setmName(String mName) {
 		this.mName = mName;
 	}
+
 	public String getmGender() {
 		return mGender;
 	}
+
 	public void setmGender(String mGender) {
 		this.mGender = mGender;
 	}
+
 	public String getmEmail() {
 		return mEmail;
 	}
+
 	public void setmEmail(String mEmail) {
 		this.mEmail = mEmail;
 	}
+
 	public int getmSub() {
 		return mSub;
 	}
+
 	public void setmSub(int mSub) {
 		this.mSub = mSub;
 	}
+
 	public int getmSuber() {
 		return mSuber;
 	}
+
 	public void setmSuber(int mSuber) {
 		this.mSuber = mSuber;
 	}
+
 	public MultipartFile getmProfile() {
 		return mProfile;
 	}
+
 	public void setmProfile(MultipartFile mProfile) {
 		this.mProfile = mProfile;
 	}
+
 	public String getmProfileName() {
 		return mProfileName;
 	}
+
 	public void setmProfileName(String mProfileName) {
 		this.mProfileName = mProfileName;
 	}
+
+	public String getmNickName() {
+		return mNickName;
+	}
+
+	public void setmNickName(String mNickName) {
+		this.mNickName = mNickName;
+	}
+
+	public int getmPoint() {
+		return mPoint;
+	}
+
+	public void setmPoint(int mPoint) {
+		this.mPoint = mPoint;
+	}
+
 	@Override
 	public String toString() {
-		return "MemberDTO [mId=" + mId + ", mPw=" + mPw + ", mName=" + mName + ", mGender=" + mGender + ", mEmail="
-				+ mEmail + ", mSub=" + mSub + ", mSuber=" + mSuber + ", mProfile=" + mProfile + ", mProfileName="
-				+ mProfileName + "]";
+		return "MemberDTO{" +
+				"mId='" + mId + '\'' +
+				", mPw='" + mPw + '\'' +
+				", mName='" + mName + '\'' +
+				", mGender='" + mGender + '\'' +
+				", mEmail='" + mEmail + '\'' +
+				", mSub=" + mSub +
+				", mSuber=" + mSuber +
+				", mProfile=" + mProfile +
+				", mProfileName='" + mProfileName + '\'' +
+				", mNickName='" + mNickName + '\'' +
+				", mPoint=" + mPoint +
+				'}';
 	}
-	
-	
 }
--- src/main/java/com/icia/musicwired/dto/uploadDto.java
+++ src/main/java/com/icia/musicwired/dto/uploadDto.java
@@ -15,103 +15,116 @@ public class uploadDto {
     private String muName;
     private String muGanre;  //장르
     private Date muDate;
-    private String muImage;
+    private String muImage; //포스터 이름
     private String muLyrics; //가사
     private int muHit;
-    private int muLike;
     private int muPrice;
-    private String mFile;
-
-	private int heartImg; //좋아요 하트 이미지
+    private String mFile; //음악 파일 이름
     private MultipartFile muFile;  //음원
     private MultipartFile mImage;  //포스터
+
+	public int getMuCode() {
+		return muCode;
+	}
+
+	public void setMuCode(int muCode) {
+		this.muCode = muCode;
+	}
+
 	public String getMuSinger() {
 		return muSinger;
 	}
+
 	public void setMuSinger(String muSinger) {
 		this.muSinger = muSinger;
 	}
+
+	public String getmId() {
+		return mId;
+	}
+
+	public void setmId(String mId) {
+		this.mId = mId;
+	}
+
 	public String getMuName() {
 		return muName;
 	}
+
 	public void setMuName(String muName) {
 		this.muName = muName;
 	}
+
 	public String getMuGanre() {
 		return muGanre;
 	}
+
 	public void setMuGanre(String muGanre) {
 		this.muGanre = muGanre;
 	}
+
 	public Date getMuDate() {
 		return muDate;
 	}
+
 	public void setMuDate(Date muDate) {
 		this.muDate = muDate;
 	}
+
 	public String getMuImage() {
 		return muImage;
 	}
+
 	public void setMuImage(String muImage) {
 		this.muImage = muImage;
 	}
+
 	public String getMuLyrics() {
 		return muLyrics;
 	}
+
 	public void setMuLyrics(String muLyrics) {
 		this.muLyrics = muLyrics;
 	}
+
 	public int getMuHit() {
 		return muHit;
 	}
+
 	public void setMuHit(int muHit) {
 		this.muHit = muHit;
 	}
-	public int getMuLike() {
-		return muLike;
-	}
-	public void setMuLike(int muLike) {
-		this.muLike = muLike;
-	}
+
 	public int getMuPrice() {
 		return muPrice;
 	}
+
 	public void setMuPrice(int muPrice) {
 		this.muPrice = muPrice;
-	}
-	public MultipartFile getMuFile() {
-		return muFile;
-	}
-	public void setMuFile(MultipartFile muFile) {
-		this.muFile = muFile;
-	}
-	public MultipartFile getmImage() {
-		return mImage;
-	}
-	public void setmImage(MultipartFile mImage) {
-		this.mImage = mImage;
 	}
 
 	public String getmFile() {
 		return mFile;
 	}
+
 	public void setmFile(String mFile) {
 		this.mFile = mFile;
 	}
 
-	public int getMuCode() {
-		return muCode;
-	}
-	public void setMuCode(int muCode) {
-		this.muCode = muCode;
+	public MultipartFile getMuFile() {
+		return muFile;
 	}
 
-	public String getmId(){
-		return mId;
+	public void setMuFile(MultipartFile muFile) {
+		this.muFile = muFile;
 	}
 
-	public void setmId(){
-		this.mId=mId;
+	public MultipartFile getmImage() {
+		return mImage;
+	}
+
+	public void setmImage(MultipartFile mImage) {
+		this.mImage = mImage;
 	}
 
 	@Override
@@ -126,22 +139,10 @@ public class uploadDto {
 				", muImage='" + muImage + '\'' +
 				", muLyrics='" + muLyrics + '\'' +
 				", muHit=" + muHit +
-				", muLike=" + muLike +
 				", muPrice=" + muPrice +
 				", mFile='" + mFile + '\'' +
-				", heartImg=" + heartImg +
 				", muFile=" + muFile +
 				", mImage=" + mImage +
 				'}';
 	}
-
-	public int getHeartImg() {
-		return heartImg;
-	}
-	public void setHeartImg(int heartImg) {
-		this.heartImg = heartImg;
-	}
-
-
-
 }
