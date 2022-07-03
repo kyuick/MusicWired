5b68cabee453f3614fb1fcc5491b806d04c0a566
minsu <minsu@DESKTOP-52O33HU>
2022-07-04 08:56:34 +0900

    7/4 jms

--- src/main/java/com/icia/musicwired/service/searchService.java
+++ src/main/java/com/icia/musicwired/service/searchService.java
@@ -2,10 +2,18 @@ package com.icia.musicwired.service;
 
 import org.springframework.web.servlet.ModelAndView;
 
+import com.icia.musicwired.dto.serchDTO;
+
 
 
 public interface searchService {
 
 	ModelAndView search(String keyword);
+
+	ModelAndView songSearch(String keyword,int page,int limit);
+
+	ModelAndView singerSearch(String keyword,int page,int limit);
+
+	ModelAndView test(serchDTO search); //테스트
 
 }
