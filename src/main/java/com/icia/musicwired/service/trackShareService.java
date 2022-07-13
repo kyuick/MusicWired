daf1b3ed208ecff03de81be5255810e52dfb2647
ysp13 <ysp13@ysp>
2022-07-13 09:04:10 +0900

    track 컨트롤러 추가 , playlist ajax 추가, board 추가

---
+++ src/main/java/com/icia/musicwired/service/trackShareService.java
@@ -0,0 +1,19 @@
+package com.icia.musicwired.service;
+
+import java.util.List;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.icia.musicwired.dto.trackShareDTO;
+
+public interface trackShareService {
+
+	List<trackShareDTO> trackShare(String tsMid);
+
+	List<trackShareDTO> trackShareSearch(String tsMid);
+
+	ModelAndView track();
+
+	ModelAndView playListView2(String tsMid);
+
+}
